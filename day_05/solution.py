9873316ff6fa115aeb1a1565c156f521fa511654
Nirmit Shah <nirmit@squareup.com>
2023-12-24 22:47:03 -0600

    day 5 part 2 finally

--- day_05/solution.py
+++ day_05/solution.py
@@ -1,8 +1,9 @@
 import re
 import math
 from collections import defaultdict
-f = [x for x in open("input.txt").read().strip().split("\n\n")]
+from tqdm import tqdm, trange
 
+f = [x for x in open("input.txt").read().strip().split("\n\n")]
 
 
 def buildMaps(f):
@@ -15,6 +16,7 @@ def buildMaps(f):
         maps.append(q)
     return maps
 
+
 def traverse(x, m):
     for dr, sr, c in m:
         if sr <= x < sr + c:
@@ -25,8 +27,42 @@ def traverse(x, m):
 seeds = [int(x) for x in (f[0].split(": ")[1]).split()]
 q = math.inf
 maps = buildMaps(f)
+
 for seed in seeds:
     for m in maps:
         seed = traverse(seed, m)
     q = min(q, seed)
 print(q)
+
+seed_ranges = []
+for i in range(0, len(seeds), 2):
+    seed_ranges.append((seeds[i], seeds[i + 1]))
+
+def superTraverse(x):
+    for m in maps:
+        x = traverse(x, m)
+    return x
+
+
+def recurse(low, dist):
+    if dist == 1:
+        return min(superTraverse(low), superTraverse(low + 1))
+    step = dist // 2
+    middle = low + step
+
+    start = superTraverse(low)
+    mid = superTraverse(middle)
+    high = superTraverse(low + dist)
+
+    found = math.inf
+    if start + step != mid:
+        found = recurse(low, step)
+    if mid + dist - step != high:
+        found = min(found, recurse(middle, dist - step))
+    return found
+
+
+superMin = math.inf
+for i, seed_range in enumerate(seed_ranges):
+    superMin = min(superMin, recurse(seed_range[0], seed_range[1]))
+print(superMin)
